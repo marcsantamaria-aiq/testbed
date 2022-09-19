135c63f2c6a375e15e6dce28fa1948932934edcf
marcsantamaria-aiq <75801282+marcsantamaria-aiq@users.noreply.github.com>
2022-09-19 17:47:16 +0200

    Update script1.ps1

--- script1.ps1
+++ script1.ps1
@@ -16,7 +16,7 @@ $agent_token =  $response.token
 $fd_url = $fd_url.ToLower() -replace 'https://',''
 $arguments = "/S /AiInstDir=`"C:\Program Files\AttackIQ\FiredrillAgent\`" /ConsoleServerAddress=`"$fd_url`" /ConsoleServerPort=`"443`" /UseHttps=`"1`" /AuthenticationToken=`"$agent_token`" /ProxyHttpScheme=`"http://`" /ProxyHttpsScheme=`"http://`" /AuthToken=`"$agent_token`" /PlatformAddress=`"$fd_url`" /PlatformPort=`"443`" "
 Start-Process $ai_install_dir\*.exe -ArgumentList $arguments
-Start-Sleep -Seconds 30
+Start-Sleep -Seconds 60
 $service = Get-Service -Name "AttackIQ Testpoint Execute Service" -ErrorAction SilentlyContinue
 if ($service.Length -gt 0) {
   exit 0
